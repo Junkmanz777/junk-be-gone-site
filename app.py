2af22157b3dbcba11a4ac802a421ac2b08d1bbef
Junkmanz777 <anthonyspam7777@gmail.com>
2026-06-23 13:31:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+
+@app.route("/free-stuff")
+def free_stuff():
+    return render_template("free_stuff.html")
