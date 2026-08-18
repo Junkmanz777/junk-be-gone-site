8f2ad30e56f50159de8786a9de67962676213de5
Junkmanz777 <anthonyspam7777@gmail.com>
2026-08-18 09:15:41 -0600

    Simplify homepage render path

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    featured_video_path = os.path.join(app.static_folder, "featured-junk-be-gone.mp4")
-    return render_template(
-        "index.html",
-        featured_video_exists=os.path.isfile(featured_video_path),
-    )
+    return render_template("index.html")
+
 
 @app.route("/free-stuff")
 def free_stuff():
     return render_template("free_stuff.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
