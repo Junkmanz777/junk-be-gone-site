e5d4e0bd6bbea2add6867b3e8aa727cc8503039e
Junkmanz777 <anthonyspam7777@gmail.com>
2026-08-19 14:18:26 -0600

    Make junkbegonenm.com the canonical domain

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 import os
 
 app = Flask(__name__)
+
+CANONICAL_HOST = "junkbegonenm.com"
+REDIRECT_HOSTS = {
+    "junkbegoneclovis.com",
+    "www.junkbegoneclovis.com",
+    "www.junkbegonenm.com",
+}
+
+
+@app.before_request
+def redirect_to_canonical_domain():
+    host = request.host.split(":", 1)[0].lower()
+    if host not in REDIRECT_HOSTS:
+        return None
+
+    target = f"https://{CANONICAL_HOST}{request.path}"
+    if request.query_string:
+        target += f"?{request.query_string.decode()}"
+
+    return redirect(target, code=301)
 
 
 @app.route("/")
